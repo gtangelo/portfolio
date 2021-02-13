7cb201c11ef581cb5d6ebe221694d282728d95d7
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2021-02-13 21:21:58 +1100

    fix navbar for mobile screen

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,54 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
+import { Container } from "styled-components/Container";
 import "./index.css";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
   const [menuIcon, setMenuIcon] = useState(false);
 
-  const handleMenu = () => setMenuIcon(!menuIcon);
+  const handleMenu = () => setMenuIcon(prevState => !prevState);
   const closeMenu = () => setMenuIcon(false);
+  console.log(menuIcon);
 
   return (
     <nav>
-      <div className="logo">
+      <div className="icon">
         <Link to="/">
-          <img src="/favicon.ico" alt="Gabriel Ting" width="60px" />
+          <img src="/favicon.ico" alt="Gabriel Ting"/>
         </Link>
+        {menuIcon ? (
+          <AiOutlineClose size="30px" className="menu" onClick={handleMenu} />
+        ) : (
+          <AiOutlineMenu size="30px" className="menu" onClick={handleMenu} />
+        )}
       </div>
-      <Link to="/about" className="nav-link" onClick={closeMenu}>
+      <Link
+        to="/about"
+        className={menuIcon ? "nav-link" : "nav-link none"}
+        onClick={closeMenu}
+      >
         About
       </Link>
-      <Link to="/project" className="nav-link" onClick={closeMenu}>
+      <Link
+        to="/project"
+        className={menuIcon ? "nav-link" : "nav-link none"}
+        onClick={closeMenu}
+      >
         Projects
       </Link>
-      <Link to="/blog" className="nav-link" onClick={closeMenu}>
+      <Link
+        to="/blog"
+        className={menuIcon ? "nav-link" : "nav-link none"}
+        onClick={closeMenu}
+      >
         Blog
       </Link>
-      <Link to="/resume" className="nav-link" onClick={closeMenu}>
+      <Link
+        to="/resume"
+        className={menuIcon ? "nav-link" : "nav-link none"}
+        onClick={closeMenu}
+      >
         Resume
       </Link>
     </nav>
